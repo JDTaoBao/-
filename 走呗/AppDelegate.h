b1407898135251b8fe3d5da399ed290b6832762a
apple <apple@appledeMac-mini-2.local>
2015-09-16 10:05:04 +0800

    Initial Commit

---
+++ 走呗/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  走呗
+//
+//  Created by apple on 15/9/16.
+//  Copyright (c) 2015年 JIUXIAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 走呗/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  走呗
+//
+//  Created by apple on 15/9/16.
+//  Copyright (c) 2015年 JIUXIAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
